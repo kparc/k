3ead12996805a0f218c9ee82202843c0c1c3b118
Alexander Belopolsky <a@enlnt.com>
2019-08-12 22:23:52 -0400

    [+] define travis ci job

--- get.sh
+++ get.sh
@@ -8,11 +8,17 @@ printf "downloading $(basename $u) from anaconda.org..."
 rm -rf "/tmp/$b" && mkdir "/tmp/$b"
 curl -LGs $u | tar --directory "/tmp/$b" -jxf - "info/LICENSE.txt" "bin/k" && printf "done.\n\n"
 COLUMNS=79
-eval $(resize)
-fmt -w $(($COLUMNS - 4)) "/tmp/$b/info/LICENSE.txt"
+if [ -z "$CI" ]  && [ -t 0 ]; then
+    eval $(resize)
+    fmt -w $(($COLUMNS - 4)) "/tmp/$b/info/LICENSE.txt"
+    read=read
+else
+    read=true
+    input=y
+fi
 while true
 do
-    read -r -p "Do you agree with the terms of the Evaluation Agreement? [Y/n] " input
+    $read -r -p "\nDo you agree with the terms of the Evaluation Agreement? [Y/n] " input
     case $input in
         [yY][eE][sS]|[yY])
     chmod +x /tmp/$b/bin/k
